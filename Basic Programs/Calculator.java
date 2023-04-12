f417d0ef2570f568d6c82ff91f26735b1943f6cd
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2023-04-12 23:45:28 +0530

    Update Calculator.java

--- Basic Programs/Calculator.java
+++ Basic Programs/Calculator.java
@@ -3,6 +3,10 @@ class Calculator
 {
   public static void main(string arg[])
   {
-    
+      int a,b;
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter Two No.: ");
+      a = sc.nextInt();
+      b = sc.nextInt();
   }
 }
