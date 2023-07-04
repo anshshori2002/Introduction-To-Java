9a2287c5413d1c6a16cd1799a3b3731c9d6e7575
ANSH SHORI <84807307+anshshori2002@users.noreply.github.com>
2023-07-04 23:07:12 +0530

    Update Addition.java

--- Basic Programs/Addition.java
+++ Basic Programs/Addition.java
@@ -9,7 +9,7 @@ class Addition
 }
 class Main
 {
-    public static void main(String[] args)
+    public static void main(String []args)
     {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter First Number: ");
